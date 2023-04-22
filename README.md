64c9f5e7c3b08194aec30a6a44ad1c856621970d
Tashy <office@pluscommunication.eu>
2023-04-22 17:59:10 +0300

    Article setup

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# convergentaSM
+# From Likes to Change: Assessing the Impact of Citizen Engagement on the European Commission's Social Media Platforms
+
+#Obiectiv
+The overall objective of this research is to analyze and compare the online engagement of the public on various official social media platforms (Facebook, Instagram, Twitter, and YouTube) of the European Commission.
+
+# Variabile
+- Type - tip de postare (categ. nominala)
+- Date - data postarii
+- Likes
+- Comments
+- Shares
+- Fans - numar de urmaritori
+- Page - social media (categ. nominala)
+- mean_sentiment - scorul la sentiment (vezi ca sunt 4 dictionare)
+
+Variabile compozit
+- Reactions = Likes + Comments + Shares
+- ER = Reactions / Fans
+
+
+# Ipoteze
+Avem asa:
+- Sa ne legam de evenimente cu impact emotional - invazia 24.02,2022 si bucha - 8 04 2022
+
+H1 - Comunicarile cu impact emotional negativ sunt asociate cu un angajament ridicat
+H2 - Platformele de comunicare modereaza efectul impactului emotional asupra angajamentului 
+H3 - In cat timp Influenta comunicarii cu impact emotional negativ asupra angajamentului se normalizeaza
+H3 - Nu stiu cum naiba sa formulam o ipoteza care sa implice o serie temporala.????
--- analiza.R
+++ analiza.R
@@ -1,22 +1,18 @@
 #Rulare####
-library(rtweet);library(httpuv);library(writexl);library(tidytext);library(tidyverse);library(readxl);library(tm);library(textdata);library("SnowballC");library("wordcloud");library("RColorBrewer");library("syuzhet");library("ggplot2");library(lubridate);library(tm);library(SentimentAnalysis);library(quanteda);library(wordcloud2)
-library(ggraph);library(igraph)
-library(revtools)
-library(scales)
-library(SentimentAnalysis)
-library(dplyr)
+library(writexl);library(tidytext);library(tidyverse);library(readxl);library(tm);library(textdata);library("SnowballC");library("wordcloud");library("RColorBrewer");library("syuzhet");library("ggplot2");library(lubridate);library(tm);library(scales);library(SentimentAnalysis);library(dplyr)
+library(lubridate)
+#Importare date####
 
+facebook <- read_xlsx("facebook.xlsx"); facebook$Page <- "Facebook"; facebook$ER = facebook$Reactions/facebook$Fans
+twitter <- read_xlsx("twitter.xlsx"); twitter$Page <- "Twitter"; twitter$ER = twitter$Reactions/twitter$Fans
+instagram <- read_xlsx("instagram.xlsx"); instagram$Page <- "Instagram"; instagram$ER = instagram$Reactions/instagram$Fans
+youtube <- read_xlsx("youtube.xlsx"); youtube$Page <- "Youtube"; youtube$ER = youtube$Reactions/youtube$Fans
+date <- rbind(facebook,twitter, youtube,instagram); date$ER = date$Reactions/date$Fans
 
-facebook <- read_xlsx("facebook.xlsx"); facebook$Page <- "Facebook"; facebook$ER = facebook$Reactions/facebook$Fans*100
-twitter <- read_xlsx("twitter.xlsx"); twitter$Page <- "Twitter"; twitter$ER = twitter$Reactions/twitter$Fans*100
-instagram <- read_xlsx("instagram.xlsx"); instagram$Page <- "Instagram"; instagram$ER = instagram$Reactions/instagram$Fans*100
-youtube <- read_xlsx("youtube.xlsx"); youtube$Page <- "Youtube"; youtube$ER = youtube$Reactions/youtube$Fans*100
-date <- rbind(facebook,twitter, youtube,instagram); date$ER = date$Reactions/date$Fans*100
+#Salvare set de date####
+write_xlsx(x = date, path = "fbbucha.xlsx", col_names = TRUE)
 
-write_xlsx(x = date, path = "date.xlsx", col_names = TRUE)
-
-ggplot(date, aes(Date, ER, fill=Page)) + geom_area()
-
+#Nr. postari / canal####
 date %>%
   group_by(Page) %>%
   dplyr::summarise(n = n()) %>%
@@ -39,27 +35,30 @@ date_sentiment <- dplyr::select(date_sentiment,
                                 SentimentGI, SentimentHE,
                                 SentimentLM, SentimentQDAP, 
                                 WordCount)
-date_sentiment <- dplyr::mutate(date_sentiment, 
-                                mean_sentiment = rowMeans(date_sentiment[,-5]))
-date_sentiment <- dplyr::select(date_sentiment, 
-                                WordCount, 
-                                mean_sentiment)
+#date_sentiment <- dplyr::mutate(date_sentiment, mean_sentiment = rowMeans(date_sentiment[,-5]))
+# date_sentiment <- dplyr::select(date_sentiment, 
+#                                 WordCount, 
+#                                 mean_sentiment)
 date <- cbind.data.frame(date, date_sentiment)
-date_negative <- filter(date, mean_sentiment < 0); date_negative$tone <- "negative"
-date_pozitive <- filter(date, mean_sentiment > 0); date_pozitive$tone <- "pozitive"
-date <- rbind(date_negative, date_pozitive)
-ggplot(date, aes(created_at, mean_sentiment)) + geom_bin2d(colour="blue") + 
+# date_negative <- filter(date, mean_sentiment < 0); date_negative$tone <- "negative"
+# date_pozitive <- filter(date, mean_sentiment > 0); date_pozitive$tone <- "pozitive"
+# date <- rbind(date_negative, date_pozitive)
+ggplot(date, aes(Date, SentimentGI)) + geom_bin2d(colour="blue") + 
   scale_fill_gradient(low="light blue", high="dark blue") +
   geom_hline(yintercept=0, linetype="dashed", color = "red", size = 1.5) +
   ylab("Sentiment Indicator Average") + xlab("Months") +
-  facet_wrap(~Page, ncol = 3, scales="free_x") + theme_minimal() + theme(legend.position = "none")
+  facet_wrap(~Page, ncol = 4, scales="free_x") + theme_minimal() + theme(legend.position = "none")
 
+#plot - sentiment ER####
+ggplot(data = date, aes(x = Page, y = ER, fill=Page)) + 
+  geom_boxplot() + 
+  facet_wrap(~Page, ncol = 4, scales = "free_x")+
+  theme_minimal()
 
 
 
 #new
-library(tm)
-corpus <- Corpus(DirSource(directory="D:/R Projects/convergentaSM/Mesaje"))
+corpus <- Corpus(DirSource(directory="C:/RProjects/ConvergentaSM/Mesaje"))
 summary(corpus)
 
 #cleaning
@@ -106,15 +105,4 @@ mean_ER <- data.frame(Platforma = c("Facebook", "Instagram", "Twitter", "YouTube
 barplot(mean_ER$Media_ER, names.arg = mean_ER$Platforma, col = "blue",
         xlab = "Platforme sociale", ylab = "Media rata de angajament (ER)")
 
-library(ggplot2)
-ggplot(date, aes(x = Page, y = ER)) +
-  geom_bar(stat = "identity") +
-  labs(title = "Comparatie rata de angajament in functie de Page",
-       x = "Page",
-       y = "Engagement Rate")
-
-ggplot(data = date, aes(x = Page, y = ER, fill = tone)) + 
-  geom_boxplot() + 
-  facet_wrap(~Page, ncol = 4, scales = "free_x")+
- theme_minimal()
 
