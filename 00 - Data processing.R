70ba33ff623168c2b9378230d0eec38b08f15aee
Cristian Opariuc-Dan <copariuc@gmail.com>
2023-06-12 15:12:03 +0300

    Packages citation updated. Proofread ready

--- 00 - Data processing.R
+++ 00 - Data processing.R
@@ -1,25 +1,8 @@
 # Installing and loading libraries
 if (!require(readxl)) install.packages("readxl")
 if (!require(dplyr)) install.packages("dplyr")
-# if (!require(writexl)) install.packages("writexl")
-# if (!require(tidytext)) install.packages("tidytext")
-# if (!require(tidyverse)) install.packages("tidyverse")
-# if (!require(tm)) install.packages("tm")
-# if (!require(textdata)) install.packages("textdata")
-# if (!require(textdata)) install.packages("textdata")
-# if (!require(SnowballC)) install.packages("SnowballC")
-# if (!require(wordcloud)) install.packages("wordcloud")
-# if (!require(RColorBrewer)) install.packages("RColorBrewer")
-# if (!require(syuzhet)) install.packages("syuzhet")
-# if (!require(ggplot2)) install.packages("ggplot2")
-# if (!require(lubridate)) install.packages("lubridate")
-# if (!require(scales)) install.packages("scales")
-
 
 library(readxl); library(dplyr)
-# library(writexl); library(tidytext); library(tidyverse); library(tm); library(textdata)
-# library(SnowballC); library(wordcloud); library(RColorBrewer); library(syuzhet); library(ggplot2 )
-# library(lubridate); library(scales); library(SentimentAnalysis)
 
 # Data import
 facebook <- read_xlsx("facebook.xlsx"); facebook$Page <- "Facebook"; facebook <- select(facebook, Page, Message, Type, Date, Reactions, Likes, Comments, Shares, Fans)
